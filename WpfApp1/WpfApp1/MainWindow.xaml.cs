2c12a5b79143aed34cacf63c8c22b3046fc6825f
bogoNBG <135821011+bogoNBG@users.noreply.github.com>
2023-07-02 14:34:07 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Contact> contacts = new List<Contact>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact();
+            contact.Name = GetName.Text;
+            contact.Number = NumberBox.Text;
+            contacts.Add(contact);
+            Ukazatel.Text = String.Empty;
+            foreach(Contact c in contacts)
+            {
+                Ukazatel.Text += $"{c.Name} {c.Number}\n";
+            }
+
+        }
+    }
+}
---
+++ WpfApp1gfgfdfgdf/WpfApp1gfgfdfgdf/ViewModels/PhonebookViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp1gfgfdfgdf.Models;
+
+namespace WpfApp1gfgfdfgdf.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
+    public class PhonebookViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        private Contact selectedContact;
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged(nameof(SelectedContact));
+                //DeleteContactCommand.OnCanExecuteChanged();
+            }
+        }
+
+        public ICommand AddContactCommand { get; }
+        public ICommand DeleteContactCommand { get; }
+
+        public PhonebookViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+
+            AddContactCommand = new RelayCommand(AddContact);
+            DeleteContactCommand = new RelayCommand(DeleteContact, CanDeleteContact);
+        }
+
+        private void AddContact(object parameter)
+        {
+            Contacts.Add(new Contact());
+        }
+
+        private void DeleteContact(object parameter)
+        {
+            if (SelectedContact != null)
+                Contacts.Remove(SelectedContact);
+        }
+
+        private bool CanDeleteContact(object parameter)
+        {
+            return SelectedContact != null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+
+}
